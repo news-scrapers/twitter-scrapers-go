520c4b440d89905943601bca8652e38299dae7ee
Hugo J. Bello <hugojose@azlo.com>
2019-05-13 12:36:40 +0200

    fix

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.12
 
 require (
 	github.com/HugoJBello/go-twitter v0.0.0-20190510102508-2d319b829a18
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/antchfx/htmlquery v1.0.0 // indirect
+	github.com/antchfx/xmlquery v1.0.0 // indirect
+	github.com/antchfx/xpath v0.0.0-20190319080838-ce1d48779e67 // indirect
 	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
 	github.com/chromedp/chromedp v0.2.0
 	github.com/dghubble/go-twitter v0.0.0-20190305084156-0022a70e9bee
@@ -11,11 +15,16 @@ require (
 	github.com/dghubble/sling v1.2.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/joho/godotenv v1.3.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/stretchr/testify v1.3.0
+	github.com/temoto/robotstxt v0.0.0-20180810133444-97ee4a9ee6ea // indirect
 	github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
@@ -23,4 +32,5 @@ require (
 	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529 // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
 	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,95 +1,33 @@
-// Command keys is a chromedp example demonstrating how to send key events to
-// an element.
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/chromedp/chromedp"
-	"github.com/chromedp/chromedp/kb"
-)
-
-var (
-	flagPort = flag.Int("port", 8544, "port")
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	flag.Parse()
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
 
-	// run server
-	go testServer(fmt.Sprintf(":%d", *flagPort))
-
-	// create context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	// run task list
-	var val1, val2, val3, val4 string
-	err := chromedp.Run(ctx, sendkeys(fmt.Sprintf("http://localhost:%d", *flagPort), &val1, &val2, &val3, &val4))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("#input1 value: %s", val1)
-	log.Printf("#textarea1 value: %s", val2)
-	log.Printf("#input2 value: %s", val3)
-	log.Printf("#select1 value: %s", val4)
-}
-
-// sendkeys sends
-func sendkeys(host string, val1, val2, val3, val4 *string) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(host),
-		chromedp.WaitVisible(`#input1`, chromedp.ByID),
-		chromedp.WaitVisible(`#textarea1`, chromedp.ByID),
-		chromedp.SendKeys(`#textarea1`, kb.End+"\b\b\n\naoeu\n\ntest1\n\nblah2\n\n\t\t\t\b\bother box!\t\ntest4", chromedp.ByID),
-		chromedp.Value(`#input1`, val1, chromedp.ByID),
-		chromedp.Value(`#textarea1`, val2, chromedp.ByID),
-		chromedp.SetValue(`#input2`, "test3", chromedp.ByID),
-		chromedp.Value(`#input2`, val3, chromedp.ByID),
-		chromedp.SendKeys(`#select1`, kb.ArrowDown+kb.ArrowDown, chromedp.ByID),
-		chromedp.Value(`#select1`, val4, chromedp.ByID),
-	}
-}
-
-// testServer is a simple HTTP server that displays the passed headers in the html.
-func testServer(addr string) error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(res http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(res, indexHTML)
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
 	})
-	return http.ListenAndServe(addr, mux)
-}
 
-const indexHTML = `<!doctype html>
-<html>
-<head>
-  <title>example</title>
-</head>
-<body>
-  <div id="box1" style="display:none">
-    <div id="box2">
-      <p>box2</p>
-    </div>
-  </div>
-  <div id="box3">
-    <h2>box3</h3>
-    <p id="box4">
-      box4 text
-      <input id="input1" value="some value"><br><br>
-      <textarea id="textarea1" style="width:500px;height:400px">textarea</textarea><br><br>
-      <input id="input2" type="submit" value="Next">
-      <select id="select1">
-        <option value="one">1</option>
-        <option value="two">2</option>
-        <option value="three">3</option>
-        <option value="four">4</option>
-      </select>
-    </p>
-  </div>
-</body>
-</html>`
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
+}
